472a397a6ea5686c5cff881d3e3bd162e63c726f
Fathan Maulana <94539481+fathanmaul@users.noreply.github.com>
2025-08-02 23:05:34 +0700

    feat(master): add master models for Genre, Licensor, Producer, Source, and Studio

---
+++ app/Models/Master/Genre.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Master;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Genre extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "genres";
+}
---
+++ app/Models/Master/Licensor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Master;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Licensor extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "licensors";
+}
