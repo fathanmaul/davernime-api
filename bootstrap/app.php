75c3ced1dc571069a481afba4fb60b452e7c2dd4
Fathan Maulana <94539481+fathanmaul@users.noreply.github.com>
2025-08-11 00:02:32 +0700

    refactor(app): improve exception handling in application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -7,12 +8,13 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Illuminate\Validation\UnauthorizedException;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -32,6 +34,12 @@ return Application::configure(basePath: dirname(__DIR__))
                     return new JsonResponse([
                         'message' => $e->getMessage(),
                     ], Response::HTTP_FORBIDDEN);
+                }
+
+                if ($e instanceof NotFoundHttpException) {
+                    return new JsonResponse([
+                        'message' => app()->isLocal() ? $e->getMessage() : 'Not found.',
+                    ], Response::HTTP_NOT_FOUND);
                 }
 
                 return new JsonResponse([
