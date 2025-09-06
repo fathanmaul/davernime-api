742cb22f4fcd0a570355b685180d10c1cd1a95eb
fathanmaul <fthnmrh012@gmail.com>
2025-09-06 22:22:31 +0700

    fix(route): adding user route to fetch user data

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,8 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+Route::get('/user', function () {
+    return auth()->user();
+})->middleware('auth:sanctum');
+
 require __DIR__.'/auth.php';
